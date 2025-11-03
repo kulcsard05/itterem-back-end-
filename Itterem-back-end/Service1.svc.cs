612514552e47354d96c7aae94d3281720bb51c60
kulcsard <kulcsdenes005@gmail.com>
2025-11-03 07:40:30 +0100

    Back end alap séma
    könyvtárak hozzá adása

---
+++ Itterem-back-end/Service1.svc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Itterem_back_end
+{
+   
+    
+}
