cb9e27eccd86f15d553b121b62f28976d3cc4fdf
kulcsard <kulcsdenes005@gmail.com>
2026-01-24 12:04:10 +0100

    CRUD végpontok hozzáadása a KeszetelekControllerhez
    GET, POST, PUT és DELETE végpontok a KeszetelekControllerben a 'Keszetelek' kezeléséhez.

--- vizsgaremek/Controllers/KeszetelekController.cs
+++ vizsgaremek/Controllers/KeszetelekController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices.Marshalling;
+using vizsgaremek.Modells;
 
 namespace vizsgaremek.Controllers
 {
@@ -7,5 +10,101 @@ namespace vizsgaremek.Controllers
     [ApiController]
     public class KeszetelekController : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetKeszetelek()
+        {
+            using (var cx = new BackEndAlapContext())
+            {
+                try
+                {
+                    var response = await cx.Keszeteleks.ToListAsync();
+                    return Ok(response);
+                }
+                catch(Exception ex)  
+                {
+                    return BadRequest(ex);
+                }
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostKeszetelek(Keszetelek keszetel)
+        {
+            using (var cx = new BackEndAlapContext())
+            {
+                try
+                {
+                    if (cx.Keszeteleks.FirstOrDefault(f => f.Nev == keszetel.Nev) != null)
+                    {
+                        return Ok("Létezik ilyen készétel!");
+                    }
+
+                    await cx.Keszeteleks.AddAsync(keszetel);
+                    await cx.SaveChangesAsync();
+                    return Ok("Sikeres készétel Mentés");
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex);
+                }
+
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, string? nev, string? leiras, int? elerheto)
+        {
+            using (var cx = new BackEndAlapContext())
+            {
+                try
+                {
+                    var keszetel = await cx.Keszeteleks.FirstOrDefaultAsync(k => k.Id == id);
+                    if (keszetel == null)
+                    {
+                        return NotFound("Nincs ilyen készétel!");
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(nev))
+                    {
+                        keszetel.Nev = nev;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(leiras))
+                    {
+                        keszetel.Leiras = leiras;
+                    }
+
+                    if (elerheto.HasValue)
+                    {
+                        keszetel.Elerheto = elerheto.Value;
+                    }
+
+                    await cx.SaveChangesAsync();
+                    return Ok("Sikeres készétel módosítás");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteKEszetel(int id)
+        {
+            using(var cx = new BackEndAlapContext())
+            {
+                try
+                {
+                    Keszetelek keszetel = new Keszetelek { Id = id };
+                    cx.Remove(keszetel);
+                    await cx.SaveChangesAsync();
+                    return Ok("Sikeres Készétel törlés.");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
     }
 }
--- vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
+++ vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eb6a2a7325cfe32c3ebd92ca1c7a552b9c1d9f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e108c844b65f943918696ddc501e7b4810b8e08")]
 [assembly: System.Reflection.AssemblyProductAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
