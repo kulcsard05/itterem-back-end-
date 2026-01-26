bc3b1663ec4df2037b8f76f0bf202a68a4614114
kulcsard <kulcsdenes005@gmail.com>
2026-01-26 10:52:35 +0100

    Jogosultságok CRUD
    Jogosultságok CRUD

---
+++ vizsgaremek/Controllers/JogosultsagController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vizsgaremek.Modells;
+
+namespace vizsgaremek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogosultsagController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetJogosultsagok()
+        {
+            using (var cx = new BackEndAlapContext())
+            {
+                try
+                {
+                    var response = await cx.Jogoks.ToListAsync();
+                    return Ok(response);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+        [HttpPost]
+        public IActionResult PostJogosultsag(Jogok jog)
+        {
+
+            using (var cx = new BackEndAlapContext())
+            {
+
+                try
+                {
+                    if (cx.Keszeteleks.FirstOrDefault(f => f.Nev == jog.Nev) != null)
+                    {
+                        return Ok("Létezik ilyen készétel!");
+                    }
+
+
+                    cx.Jogoks.Add(jog);
+                    cx.SaveChanges();
+                    return Ok("Sikeres Jogosultág Mentés");
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex);
+                }
+            }
+
+
+        }
+        [HttpPut]
+
+
+        public async Task<IActionResult> Put(int id, string? nev, string? leiras)
+        {
+            using (var cx = new BackEndAlapContext())
+            {
+                try
+                {
+                    var Jog = await cx.Jogoks.FirstOrDefaultAsync(k => k.Id == id);
+                    if (Jog == null)
+                    {
+                        return NotFound("Nincs ilyen Jogosultág!");
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(nev))
+                    {
+                        Jog.Nev = nev;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(leiras))
+                    {
+                        Jog.Leiras = leiras;
+                    }
+
+                    await cx.SaveChangesAsync();
+                    return Ok("Sikeres Jogosultág módosítás");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteJogosultsag(int id)
+        {
+            using (var cx = new BackEndAlapContext())
+            {
+                try
+                {
+                    Jogok jog = new Jogok { Id = id };
+                    cx.Remove(jog);
+                    await cx.SaveChangesAsync();
+                    return Ok("Sikeres Jogosultág törlés.");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
+
--- vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
+++ vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e108c844b65f943918696ddc501e7b4810b8e08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb9e27eccd86f15d553b121b62f28976d3cc4fdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
