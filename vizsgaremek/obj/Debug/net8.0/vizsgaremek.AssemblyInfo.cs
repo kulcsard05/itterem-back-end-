700eccd0c4452e7f09fa978603411b65756b6d66
kulcsard <kulcsdenes005@gmail.com>
2026-02-25 12:27:04 +0100

    inkontisztenciák kijavítása kommentek eltávolítása kód tisztítása

--- vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
+++ vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+313d77577115a2c6579f1104e16c4bba5fc9a048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ab19ee82d65d2ca712134c5545040f72430b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
