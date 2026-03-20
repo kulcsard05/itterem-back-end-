78a725be5b99565d3038967538ba957e96cbd6b9
kulcsard <kulcsdenes005@gmail.com>
2026-03-20 11:35:59 +0100

    nginx config

--- vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
+++ vizsgaremek/obj/Debug/net8.0/vizsgaremek.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc7fcd8dae9719e40293ea9655eb24dc6099c729")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ec28ec1f5c51604675f033296131da21fb2af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vizsgaremek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
